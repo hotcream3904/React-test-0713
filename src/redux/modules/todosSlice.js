789d636cc65db6e984ca251fd0ec7967b10f8065
hotcream3904 <dkssud6757@naver.com>
2023-07-13 11:44:22 +0900

    완료

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from "../../utils";
+
+export const __addToDo = createAsyncThunk("__addToDo", async (payload, thunkAPI) => {
+	setTimeout(() => {
+		thunkAPI.dispatch(addTodo(payload));
+	}, 2000);
+});
+
+export const __deleteTodo = createAsyncThunk("__deleteToDo", async (payload, thunkAPI) => {
+	setTimeout(() => {
+		thunkAPI.dispatch(deleteTodo(payload));
+	}, 2000);
+});
+
+const initialState = {
+	list: [],
+};
+
+const todosSlice = createSlice({
+	name: "todos",
+	initialState,
+	reducers: {
+		addTodo: (state, action) => {
+			return { ...state, list: [...state.list, action.payload] };
+		},
+		deleteTodo: (state, action) => {
+			return { ...state, list: state.list.filter((todo) => todo.id !== action.payload) };
+		},
+	},
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+// export const waitTwoSeconds = () =>
+//   new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve();
+//     }, 2000);
+//   });
+export const waitTwoSeconds = () =>
+	new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, 2000);
+	});
